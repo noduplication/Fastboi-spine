0804474de8cc9aa5e41a4828b731df64ec831396
noduplication <157242043+noduplication@users.noreply.github.com>
2024-01-26 14:32:22 -0500

    Create ram-management-error.sh

---
+++ raspi-download/ram-management-error.sh
@@ -0,0 +1,9 @@
+sudo apt update
+echo "Application closed do to ram error"
+echo "Re installing system..."
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/noduplication/Fastboi-spine.git
+git push -u origin main
