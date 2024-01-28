1b8ba8ead0b685f693707ca3c585ddd208d0de86
noduplication <157242043+noduplication@users.noreply.github.com>
2024-01-28 01:00:47 -0500

    Create macbook_install.sh

---
+++ macbook_install.sh
@@ -0,0 +1,7 @@
+echo "Drivers" /Fbs/d.txt
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/noduplication/Fastboi-spine.git
+git push -u origin main
