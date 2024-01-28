12181a47416590652dbcd02f7b576ae411e6f414
noduplication <157242043+noduplication@users.noreply.github.com>
2024-01-28 00:57:16 -0500

    Create windows-download.sh

---
+++ windows-download.sh
@@ -0,0 +1,35 @@
+echo "installing for windows"
+
+echo "# Fastboi-spine" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/noduplication/Fastboi-spine.git
+git push -u origin main
+
+sudo apt update
+
+sudo !!
+
+echo "unlocking spine.exe"
+
+sudo apt upgrade
+
+echo "Installing drives for windows 10-11..."
+
+
+echo "# Fastboi-spine" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/noduplication/Fastboi-spine.git
+git push -u origin main
+
+touch /drivers/hexdump.hex
+touch drivers/hexdump.hex >> "31 31 31 31 31 31 31 31 31 31 31"
+
+echo "installed drivers and hexdump"
+
+clear
