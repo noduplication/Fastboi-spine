cbc5b8c4c87c8e084a267e44a07f6097f8ba4819
noduplication <157242043+noduplication@users.noreply.github.com>
2024-01-28 14:54:34 -0500

    Create staff-login.py

---
+++ firewall/staff-login.py
@@ -0,0 +1,6 @@
+stp = input ('Enter staff password: ')
+if stp == "70 61 73 73 77 6f 72 64 2d 69 6e 2d 68 65 78":
+  sun = input ('Staff name:')
+  if sun == "36 33 20 37 35 20 37 32 20 37 34 20 36 39 20 37 33 20 30 64 20 30 61":
+    sfa = input ('Fave animal: ')
+    print ('welcome :)')
