4aac693c526f2dbfb114c3f5f4d34aa668c89817
noduplication <157242043+noduplication@users.noreply.github.com>
2024-01-28 00:59:18 -0500

    Create ubuntu_install.sh

---
+++ ubuntu_install.sh
@@ -0,0 +1,9 @@
+echo "gitclone..."
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/noduplication/Fastboi-spine.git
+git push -u origin main
+
+echo "done"
